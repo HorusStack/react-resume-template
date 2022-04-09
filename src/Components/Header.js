eb933cf475e68cfeefebb50b701848cf385b27a2
HorusStack <41969309+HorusStack@users.noreply.github.com>
2022-04-09 16:50:37 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import VideoPlayer from "react-background-video-player";
+import videobg from "images/bg_video_web.mp4";
 
 
 class Header extends Component {
@@ -11,7 +12,7 @@ class Header extends Component {
       var description= this.props.data.description;
       var city= this.props.data.address.city;
       var banner= "images/"+this.props.data.banner;
-      var video= "images/"+this.props.data.video";
+      var video= "images/"+this.props.data.video;
       var networks= this.props.data.social.map(function(network){
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
