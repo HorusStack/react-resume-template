6db9dafb88fa16eb084efee19e771b28035775f7
HorusStack <41969309+HorusStack@users.noreply.github.com>
2022-04-09 16:48:07 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,7 +11,7 @@ class Header extends Component {
       var description= this.props.data.description;
       var city= this.props.data.address.city;
       var banner= "images/"+this.props.data.banner;
-      var video= "images/bg_video_web.mp4";
+      var video= "images/"+this.props.data.video";
       var networks= this.props.data.social.map(function(network){
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
